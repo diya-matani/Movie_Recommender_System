e5787add18f51c3105ed5e2aaadb796e2e4f6158
Diya Matani <diyamatani17@gmail.com>
2025-12-26 13:17:20 +0530

    yes

--- model/app.py
+++ model/app.py
@@ -63,12 +63,81 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+import os
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 # Cache models to prevent reloading on every interaction
 @st.cache_resource
 def load_models():
-    movie_list = pickle.load(open('movie_dict.pkl','rb'))
-    similarity_matrix = pickle.load(open('similarity.pkl','rb'))
-    return pd.DataFrame(movie_list), similarity_matrix
+    # check if pkl files exist
+    if os.path.exists('movie_dict.pkl') and os.path.exists('similarity.pkl'):
+        movie_list = pickle.load(open('movie_dict.pkl','rb'))
+        similarity_matrix = pickle.load(open('similarity.pkl','rb'))
+        return pd.DataFrame(movie_list), similarity_matrix
+    else:
+        # If models are missing (e.g. on Streamlit Cloud), generate them!
+        with st.spinner('Building model for the first time... (This takes a minute)'):
+            movies = pd.read_csv('tmdb_5000_movies.csv')
+            credits = pd.read_csv('tmdb_5000_credits.csv')
+            
+            movies = movies.merge(credits, on='title')
+            movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+            
+            def convert(obj):
+                L = []
+                try:
+                    for i in ast.literal_eval(obj):
+                        L.append(i['name'])
+                except: pass
+                return L
+            
+            def convert3(obj):
+                L = []
+                counter = 0
+                try:
+                    for i in ast.literal_eval(obj):
+                        if counter != 3:
+                            L.append(i['name'])
+                            counter += 1
+                        else: break
+                except: pass
+                return L
+            
+            def fetch_director(obj):
+                L = []
+                try:
+                    for i in ast.literal_eval(obj):
+                        if i['job'] == 'Director':
+                            L.append(i['name'])
+                            break
+                except: pass
+                return L
+
+            movies.dropna(inplace=True)
+            movies['genres'] = movies['genres'].apply(convert)
+            movies['keywords'] = movies['keywords'].apply(convert)
+            movies['cast'] = movies['cast'].apply(convert3)
+            movies['crew'] = movies['crew'].apply(fetch_director)
+            
+            movies['overview'] = movies['overview'].apply(lambda x: x.split())
+            movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
+            movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ", "") for i in x])
+            movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
+            movies['crew'] = movies['crew'].apply(lambda x: [i.replace(" ", "") for i in x])
+            
+            movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+            
+            new_df = movies[['movie_id', 'title', 'tags']]
+            new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
+            new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+            
+            cv = CountVectorizer(max_features=5000, stop_words='english')
+            vectors = cv.fit_transform(new_df['tags']).toarray()
+            similarity = cosine_similarity(vectors)
+            
+            return new_df, similarity
 
 movies, similarity = load_models()
 
